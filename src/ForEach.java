4031d1906e2aef1a518a735f5fb1e17699c1a4b8
Williancc1557 <willian.cacoelho@gmail.com>
2023-02-06 13:38:28 -0300

    forEach

---
+++ src/ForEach.java
@@ -0,0 +1,17 @@
+public class ForEach {
+    public static void main(String[] args) {
+        char[] lista = {'a', 'b', 'c'};
+
+        for (char elemento : lista) {
+            System.out.println(elemento);
+
+            /***
+             * output:
+             * a
+             * b
+             * c
+             */
+        }
+    }
+}
+
