647d7296b447fe831951905ad4b9d14affbc455d
Williancc1557 <willian.cacoelho@gmail.com>
2023-02-15 15:45:35 -0300

    javadoc

---
+++ src/Javadoc.java
@@ -0,0 +1,16 @@
+public class Javadoc {
+    public static void main(String[] args) {
+        sum(1, 2);
+    }
+
+    /**
+     *<p>That is a function to  return a sum</p>
+     * @author Willian Cavalcanti Coelho
+     * @param x first number
+     * @param y second number
+     * @return x + y
+     */
+    public static int sum(int x, int y) {
+        return x + y;
+    }
+}
