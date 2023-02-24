17c2cd7a62f5be0a05f6acb569a1c3cdf5b6d3d4
Williancc1557 <willian.cacoelho@gmail.com>
2023-02-24 14:15:22 -0300

    oop

---
+++ src/JavaClass/Cat.java
@@ -0,0 +1,6 @@
+package JavaClass;
+
+public class Cat {
+    public String name;
+    public int age;
+}
---
+++ src/JavaClass/Main.java
@@ -0,0 +1,10 @@
+package JavaClass;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+
+        cat.age = 2;
+        cat.name = "Gatito";
+    }
+}
